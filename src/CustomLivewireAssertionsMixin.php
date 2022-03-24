cf0a717f99aa65090e74cf215e687c667ca3665f
Fernando Pintabona <fernando.pintabona@pjn.gov.ar>
2022-03-24 09:11:02 -0300

    Added methods to test the opposite conditions: - assertPropertyNotWired - assertPropertyNotEntangled - assertMethodNotWired - assertMethodNotWiredToForm - assertDoesNotContainLivewireComponent - assertDoesNotContainBladeComponent - assertDoNotSeeBefore

--- src/CustomLivewireAssertionsMixin.php
+++ src/CustomLivewireAssertionsMixin.php
@@ -23,6 +23,21 @@ class CustomLivewireAssertionsMixin
         };
     }
 
+    /**
+     * Assert that the given property is NOT wired
+     */
+    public function assertPropertyNotWired(): Closure
+    {
+        return function (string $property) {
+            PHPUnit::assertDoesNotMatchRegularExpression(
+                '/wire:model(\.(defer|(lazy|debounce)(\.\d+?(ms|s)|)))*=(?<q>"|\')'.$property.'(\k\'q\')/',
+                $this->stripOutInitialData($this->lastRenderedDom)
+            );
+
+            return $this;
+        };
+    }
+
     public function assertPropertyEntangled(): Closure
     {
         return function (string $property) {
@@ -44,6 +59,30 @@ class CustomLivewireAssertionsMixin
         };
     }
 
+    /**
+     * Assert that the given property is NOT entangled
+     */
+    public function assertPropertyNotEntangled(): Closure
+    {
+        return function (string $property) {
+            $propertyRe = '('
+                . preg_quote(htmlspecialchars("'" . $property . "'", ENT_QUOTES))
+                . '|'
+                . preg_quote(htmlspecialchars('"' . $property . '"', ENT_QUOTES))
+                . '|'
+                . preg_quote('"' . $property . '"')
+                . '|'
+                . preg_quote("'" . $property . "'")
+                . ')';
+            PHPUnit::assertDoesNotMatchRegularExpression(
+                '/(@|\$wire\.)entangle\('.$propertyRe.'\)/',
+                $this->stripOutInitialData($this->lastRenderedDom)
+            );
+
+            return $this;
+        };
+    }
+
     public function assertMethodWired(): Closure
     {
         return function (string $method) {
@@ -56,10 +95,40 @@ class CustomLivewireAssertionsMixin
         };
     }
 
+    /**
+     * Assert that the given method is NOT wired
+     */
+    public function assertMethodNotWired(): Closure
+    {
+        return function (string $method) {
+            PHPUnit::assertDoesNotMatchRegularExpression(
+                '/wire:click(\.(prevent))*=(?<q>"|\')'.$method.'(\s*\(.+\)\s*)?\s*(\k\'q\')/',
+                $this->stripOutInitialData($this->lastRenderedDom)
+            );
+
+            return $this;
+        };
+    }
+
     public function assertMethodWiredToForm(): Closure
     {
         return function (string $method) {
             PHPUnit::assertMatchesRegularExpression(
+                '/wire:submit(\.(prevent))*=(?<q>"|\')'.$method.'(\k\'q\')/',
+                $this->stripOutInitialData($this->lastRenderedDom)
+            );
+
+            return $this;
+        };
+    }
+
+    /**
+     * Assert that the given method is NOT wired to form
+     */
+    public function assertMethodNotWiredToForm(): Closure
+    {
+        return function (string $method) {
+            PHPUnit::assertDoesNotMatchRegularExpression(
                 '/wire:submit(\.(prevent))*=(?<q>"|\')'.$method.'(\k\'q\')/',
                 $this->stripOutInitialData($this->lastRenderedDom)
             );
@@ -86,6 +155,27 @@ class CustomLivewireAssertionsMixin
         };
     }
 
+    /**
+     * Assert that the given Livewire component is NOT contained
+     */
+    public function assertDoesNotContainLivewireComponent(): Closure
+    {
+        return function (string $componentNeedleClass) {
+            $componentNeedle = Str::of($componentNeedleClass)
+                ->classBasename()
+                ->kebab();
+
+            $componentHaystackView = file_get_contents($this->lastRenderedView->getPath());
+
+            PHPUnit::assertDoesNotMatchRegularExpression(
+                '/@livewire\(\''.$componentNeedle.'\'|<livewire\:'.$componentNeedle.'/',
+                $componentHaystackView
+            );
+
+            return $this;
+        };
+    }
+
     public function assertContainsBladeComponent(): Closure
     {
         return function (string $componentNeedleClass) {
@@ -101,6 +191,24 @@ class CustomLivewireAssertionsMixin
         };
     }
 
+    /**
+     * Assert that the given Blade component is NOT contained
+     */
+    public function assertDoesNotContainBladeComponent(): Closure
+    {
+        return function (string $componentNeedleClass) {
+            $componentNeedle = Str::of($componentNeedleClass)
+                ->classBasename()
+                ->kebab()
+                ->prepend('<x-');
+
+            $componentHaystackView = file_get_contents($this->lastRenderedView->getPath());
+            PHPUnit::assertStringNotContainsString($componentNeedle, $componentHaystackView);
+
+            return $this;
+        };
+    }
+
     public function assertSeeBefore(): Closure
     {
         return function ($valueBefore, $valueAfter) {
@@ -109,6 +217,22 @@ class CustomLivewireAssertionsMixin
             PHPUnit::assertNotFalse($valueAfterPosition = mb_strpos($html, $valueAfter), "Value: $valueAfter not given in haystack.");
 
             PHPUnit::assertTrue($valueBeforePosition < $valueAfterPosition, "$valueBefore does occur before $valueAfter.");
+
+            return $this;
+        };
+    }
+
+    /**
+     * Assert that the given valueBefore string is NOT before valueAfter string
+     */
+    public function assertDoNotSeeBefore(): Closure
+    {
+        return function ($valueBefore, $valueAfter) {
+            $html = $this->stripOutInitialData($this->lastRenderedDom);
+            PHPUnit::assertNotFalse($valueBeforePosition = mb_strpos($html, $valueBefore), "Value: $valueBefore not given in haystack.");
+            PHPUnit::assertNotFalse($valueAfterPosition = mb_strpos($html, $valueAfter), "Value: $valueAfter not given in haystack.");
+
+            PHPUnit::assertFalse($valueBeforePosition < $valueAfterPosition, "$valueBefore does occur before $valueAfter.");
 
             return $this;
         };
