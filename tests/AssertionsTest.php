b6ee04460316e2f224a430dc1faf8855aa1651c0
Adrian Nürnberger <adrian@nuernberger.me>
2024-01-22 19:31:46 +0100

    Update AssertionsTest.php

--- tests/AssertionsTest.php
+++ tests/AssertionsTest.php
@@ -7,12 +7,8 @@ use Livewire\Livewire;
 use Livewire\LivewireServiceProvider;
 use Tests\Components\FileDownloadComponent;
 use Tests\Components\LivewireTestComponentA;
-use Tests\Components\LivewireTestComponentA;
-use Tests\Components\LivewireTestComponentB;
 use Tests\Components\LivewireTestComponentB;
 use Tests\Components\LivewireTestComponentC;
-use Tests\Components\LivewireTestComponentC;
-use Tests\View\Components\Button;
 use Tests\View\Components\Button;
 
 class AssertionsTest extends TestCase
