f8ea1247116326eec3ffe5d6dfc7086d7a1bdf7e
Aarón Gómez Méndez <aaron.gomez.mendez@gmail.com>
2026-01-14 12:30:14 -0600

    fix: add return types

--- tests/Components/FileDownloadComponent.php
+++ tests/Components/FileDownloadComponent.php
@@ -3,17 +3,18 @@
 namespace Tests\Components;
 
 use Livewire\Component;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class FileDownloadComponent extends Component
 {
-    public function streamDownload($filename = null, $headers = [])
+    public function streamDownload($filename = null, $headers = []): StreamedResponse
     {
         return response()->streamDownload(function () {
             echo 'alpinejs';
         }, $filename, $headers);
     }
 
-    public function render()
+    public function render(): string
     {
         return '<div></div>';
     }
--- tests/Components/LivewireTestComponentA.php
+++ tests/Components/LivewireTestComponentA.php
@@ -2,11 +2,12 @@
 
 namespace Tests\Components;
 
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class LivewireTestComponentA extends Component
 {
-    public function render()
+    public function render(): View
     {
         return view('livewire-test-component-a');
     }
